e0bd4624fba045cf657cf7c2f6cd66e7a2d49331
JonathanSimon098 <jonedu1130@gmail.com>
2025-08-27 00:24:20 -0500

    Implemented basic input from user.

--- yash.c
+++ yash.c
@@ -1,10 +1,25 @@
 //
-// Created by jonat on 8/25/2025.
+// ECE 461S Fall 2025
+// Section 18850
+// Jonathan Simon
+// jes7539
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello World");
+
+    char* usrInput = readline("# ");
+
+    while (usrInput  != NULL) {
+        usrInput = readline("# ");
+    }
+
+    free(usrInput);
     return 0;
 }
