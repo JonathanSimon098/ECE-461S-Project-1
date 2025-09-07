8e92d894aad2ae141cb9dc07e122816772c44bd0
JonathanSimon098 <jonedu1130@gmail.com>
2025-09-07 16:13:10 -0500

    Added capability to update job status

--- jobs.c
+++ jobs.c
@@ -1,7 +1,7 @@
 #include "jobs.h"
 #include <string.h>
 
-Node* create_job(pid_t pid, char* command, JobState state, int newjobid) {
+Node* create_job(pid_t pid, pid_t pgid, char* command, JobState state, int newjobid) {
     // Allocate memory for new node
     Node* newNode = (Node*)malloc(sizeof(Node));
     if ( newNode == NULL ) {
@@ -10,36 +10,51 @@ Node* create_job(pid_t pid, char* command, JobState state, int newjobid) {
     }
     newNode->jobid = newjobid;
     newNode->pid = pid;
+    newNode->pgid = pgid;
     newNode->command = strdup(command);
     newNode->state = state;
     newNode->next = NULL;
     return newNode;
 }
 
-void insert_job(Node** head, pid_t pid, char* command, JobState state) {
+int insert_job(Node** head, pid_t pid, pid_t pgid, char* command, JobState state) {
     int newjobid;
     if (*head == NULL) {
         newjobid = 1;
     } else {
         newjobid = (*head)->jobid + 1;
     }
-    Node* newNode = create_job(pid, command, state, newjobid);
+    Node* newNode = create_job(pid, pgid, command, state, newjobid);
     newNode->next = *head; // Points to old head
     *head = newNode; // Makes new node the HEAD (the beginning)
+    return newjobid;
+}
+
+void update_job_status(Node* head, pid_t pid, JobState state) {
+    Node* current = head;
+    Node* nextNode;
+
+    while ( current != NULL ) {
+        nextNode = current->next;
+        if ( current->pid == pid ) {
+            current->state = state;
+            break;
+        }
+        current = nextNode;
+    }
 }
 
 // Initial call to print_jobs needs to make most_recent_job equal to 1
-int print_jobs(const Node* head, int most_recent_job) {
-    if ( head == NULL ) return -1;
-    if ( head->next == NULL ) return most_recent_job;
+int print_jobs(Node** head, const Node* current, int most_recent_job) {
+    if ( current == NULL ) return most_recent_job;
 
     int is_recent_job = most_recent_job;
-    if ( is_recent_job && (head->state == STOPPED) ) {
+    if ( is_recent_job) {
         // First encounter of 'Stopped' guarantees it is at least the most recent
         is_recent_job = 0;
     }
 
-    int another_recent_job = print_jobs(head->next, is_recent_job);
+    int another_recent_job = print_jobs(head, current->next, is_recent_job);
 
     char recent_job = '+';
     if ( !most_recent_job && another_recent_job == 0 ) {
@@ -48,7 +63,7 @@ int print_jobs(const Node* head, int most_recent_job) {
     }
 
     char* state = "";
-    switch ( head->state ) {
+    switch ( current->state ) {
         case STOPPED:
             state = "Stopped";
             break;
@@ -59,7 +74,10 @@ int print_jobs(const Node* head, int most_recent_job) {
             state = "Done";
             break;
     }
-    printf("[%d]%c %s          %s\n", head->jobid, recent_job, state, head->command);
+    printf("[%d]%c %s          %s\n", current->jobid, recent_job, state, current->command);
+    if (current->state == DONE) {
+        terminate_job(head, current->jobid);
+    }
     return most_recent_job;
 }
 
@@ -106,4 +124,15 @@ void free_jobs(Node **head) {
         current = nextNode;
     }
     *head = NULL;
+}
+
+Node* find_last_stopped_job(Node* head) {
+    Node* current = head;
+    while ( current != NULL ) {
+        if ( current->state == STOPPED || current->state == RUNNING) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
 }
--- jobs.h
+++ jobs.h
@@ -18,19 +18,23 @@ typedef enum {
 typedef struct Node {
     int jobid;
     pid_t pid;
+    pid_t pgid;
     char* command;
     JobState state; // 0 for running, 1 for stopped, 2 for done
     struct Node* next;
 } Node;
 
 // Creates a new node with the given pid
-Node* create_job(pid_t pid, char* command, JobState state, int newjobid);
+Node* create_job(pid_t pid, pid_t pgid, char* command, JobState state, int newjobid);
 
 // Inserts a new job with the lowest available job id
-void insert_job(Node** head, pid_t pid, char* command, JobState state);
+int insert_job(Node** head, pid_t pid, pid_t pgid, char* command, JobState state);
+
+// Sets the job status of job liked to pid
+void update_job_status(Node* head, pid_t pid, JobState state);
 
 // Prints all the elements of the list from most recent to oldest (recursion)
-int print_jobs(const Node* head, int most_recent_job);
+int print_jobs(Node** head, const Node* current, int most_recent_job);
 
 // Frees memory and reassign pointers
 void delete_job(Node** head, Node* job_to_delete, Node* prev_job);
@@ -40,5 +44,8 @@ void terminate_job(Node** head, int jobid);
 
 // Frees all jobs available
 void free_jobs(Node** head);
+
+// Finds the job with the highest ID that is stopped
+Node* find_last_stopped_job(Node* head);
 
 #endif //ECE_461S_PROJECT_1_PIDS_H
